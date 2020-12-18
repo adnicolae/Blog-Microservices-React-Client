5a9ecdbb76007bc4e70d6d8e0b5d9b84ce51fbc0
Andrei Nicolae <ad.nicolae@outlook.com>
2020-12-18 14:29:49 +0000

    Render comment based on moderation status

--- src/components/comment-item/index.js
+++ src/components/comment-item/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Text } from 'ucc-design-system';
 
-const CommentItem = ({ comment: { content } }) => {
+const CommentItem = ({ comment: { content, status } }) => {
+  const renderComment = () => {
+    switch (status) {
+      case 'pending':
+        return 'Comment is pending moderation.';
+      case 'approved':
+        return content;
+      case 'rejected':
+        return 'Comment has been rejected. Please moderate your language.';
+    }
+  }
+
   return (
     <li>
-      <Text>{ content }</Text>
+      <Text>{ renderComment() }</Text>
     </li>
   );
 }
